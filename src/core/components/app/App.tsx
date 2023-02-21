85cf3b5da7d853d2b0b0c9c496d74771df660758
sfernandez <sfdezlop@gmail.com>
2023-02-21 18:49:01 +0100

    Change folder structure

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">202301-W06CH02_Jesus-Santiago</header>
+    </div>
+  );
+}
+
+export default App;

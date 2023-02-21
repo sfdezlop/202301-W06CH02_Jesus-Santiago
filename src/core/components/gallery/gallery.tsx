ec38a3b022f56152395e62bc8b874f0d6b89c9aa
sfernandez <sfdezlop@gmail.com>
2023-02-21 21:23:55 +0100

    Solve codesmels

--- src/core/components/gallery/gallery.tsx
+++ src/core/components/gallery/gallery.tsx
@@ -1,16 +1,16 @@
 import { CharacterStructure } from "../../models/character";
 
 export function Gallery() {
-  const { state } = useContext(PhotosContext);
+  // const { state } = useContext(PhotosContext);
 
   return (
     <>
-      <main className="gallery">
+      {/* <main className="gallery">
         {state?.photos.map((item: CharacterStructure) => (
           <div></div>
           // <Card info={item} key={item.id}></Card>
         ))}
-      </main>
+      </main> */}
     </>
   );
 }
--- src/services/repository/character.api.repo.test.ts
+++ src/services/repository/character.api.repo.test.ts
@@ -1,72 +1,70 @@
-import { CharacterApiRepo } from './character.api.repo';
+import { CharacterApiRepo } from "./character.api.repo";
 
-describe('Given the service Api repo class', () => {
-  describe('When create a new object of the class', () => {
-    test('Then it should be able to call its method', async () => {
+describe("Given the service Api repo class", () => {
+  describe("When create a new object of the class", () => {
+    test("Then it should be able to call its method", async () => {
       const mockCharacter = [
         {
-          name: 'Joffrey Baratheon',
-          family: 'Baratheon',
+          name: "Joffrey Baratheon",
+          family: "Baratheon",
           age: 19,
           isAlive: true,
-          message: 'Vais a morir todos',
-          category: 'king',
+          message: "Vais a morir todos",
+          category: "king",
           kingdomYears: 5,
-          weapon: '',
+          weapon: "",
           skill: 0,
           submission: 2,
           master: {
-            name: 'Jaime Lannister',
-            family: 'Lannister',
+            name: "Jaime Lannister",
+            family: "Lannister",
             age: 34,
             isAlive: true,
-            message: 'Primero pego y luego pregunto',
-            category: 'fighter',
+            message: "Primero pego y luego pregunto",
+            category: "fighter",
             kingdomYears: 0,
-            weapon: '',
+            weapon: "",
             skill: 0,
             submission: 2,
-            image: 'assets/jaime.jpg',
+            image: "assets/jaime.jpg",
           },
-          image: 'assets/joffrey.jpg',
+          image: "assets/joffrey.jpg",
         },
       ];
       global.fetch = jest.fn().mockResolvedValue({
         json: jest.fn().mockResolvedValue({
           characters: [
             {
-              name: 'Joffrey Baratheon',
-              family: 'Baratheon',
+              name: "Joffrey Baratheon",
+              family: "Baratheon",
               age: 19,
               isAlive: true,
-              message: 'Vais a morir todos',
-              category: 'king',
+              message: "Vais a morir todos",
+              category: "king",
               kingdomYears: 5,
-              weapon: '',
+              weapon: "",
               skill: 0,
               submission: 2,
               master: {
-                name: 'Jaime Lannister',
-                family: 'Lannister',
+                name: "Jaime Lannister",
+                family: "Lannister",
                 age: 34,
                 isAlive: true,
-                message: 'Primero pego y luego pregunto',
-                category: 'fighter',
+                message: "Primero pego y luego pregunto",
+                category: "fighter",
                 kingdomYears: 0,
-                weapon: '',
+                weapon: "",
                 skill: 0,
                 submission: 2,
-                image: 'assets/jaime.jpg',
+                image: "assets/jaime.jpg",
               },
-              image: 'assets/joffrey.jpg',
+              image: "assets/joffrey.jpg",
             },
           ],
         }),
       });
       const repo = new CharacterApiRepo();
       expect(repo).toBeInstanceOf(CharacterApiRepo);
-      // const load = await repo.loadCharacters();
-      // expect(load).toEqual(mockCharacter);
     });
   });
 });

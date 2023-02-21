c3e0c68e3fd801e99245d5a9ecb72c2b16eddd9b
sfernandez <sfdezlop@gmail.com>
2023-02-21 20:26:37 +0100

    Add repository

---
+++ src/core/components/gallery/gallery.tsx
@@ -0,0 +1,16 @@
+import { CharacterStructure } from "../../models/character";
+
+export function Gallery() {
+  const { state } = useContext(PhotosContext);
+
+  return (
+    <>
+      <main className="gallery">
+        {state?.photos.map((item: CharacterStructure) => (
+          <div></div>
+          // <Card info={item} key={item.id}></Card>
+        ))}
+      </main>
+    </>
+  );
+}

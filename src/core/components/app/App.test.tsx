3a7f1e21175a045f3d87abc4204211be56d420a9
sfernandez <sfdezlop@gmail.com>
2023-02-21 19:03:45 +0100

    Delete code smells

--- src/core/components/app/App.test.tsx
+++ src/core/components/app/App.test.tsx
@@ -14,26 +14,3 @@ test("Renders text shown", () => {
   // eslint-disable-next-line testing-library/prefer-screen-queries
   expect(getByText(/Got/i)).toBeInTheDocument();
 });
-
-// jest.mock("../header/header"); //Faltaintroducir Header folder
-// jest.mock("../app.router/approuter"); //Falta introducir AppRouter folder
-// jest.mock("../footer/footer"); //Falta introducir Footer folder
-
-// describe("Given App component", () => {
-//   beforeEach(() => {
-//     // eslint-disable-next-line testing-library/no-render-in-setup
-//     render(
-//       <Header>
-//         <></>
-//       </Header>
-//     );
-//   });
-//   describe("When it is render", () => {
-//     test("Then it should call Header, AppRouter & Footer", () => {
-//       render(<App />);
-//       expect(Header).toHaveBeenCalled();
-//       expect(AppRouter).toHaveBeenCalled();
-//       expect(Footer).toHaveBeenCalled();
-//     });
-//   });
-// });
